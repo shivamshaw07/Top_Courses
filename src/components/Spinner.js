5536d51be0a6c579019d21f6bb37d2b1916499ef
SHIVAM KUMAR SHAW <108209696+shivamshaw07@users.noreply.github.com>
2023-10-08 13:13:19 +0530

    Update Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import './Spinner.css'
 
 const Spinner = () => {
   return (
-    <div>
+    <div className='mx-auto mt-[30vh]'>
       <div className='loader'></div>
       <div className='text-white'>loading...</div>
     </div>
